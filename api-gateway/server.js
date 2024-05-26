33faf71b1be676ec0e8030514012155b8fa5b6cb
s223686707 <s223686707@deakin.edu.au>
2024-05-26 19:17:52 +1000

    updated ci-cd

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -25,7 +25,7 @@ const verifyJWT = (req, res, next) => {
   });
 };
 
-// Error handler
+// Error handlerer
 app.use((err, req, res, next) => {
   console.error('Proxy error:', err.message);
   res.status(500).json({ message: 'Proxy error', error: err.message });
